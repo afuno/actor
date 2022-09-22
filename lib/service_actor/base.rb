3b24ba1be1a12a5a0fe40adce500ca1003410a20
Anton <profox.rus@gmail.com>
2022-09-22 22:57:51 +0300

    Attempt

--- lib/service_actor/base.rb
+++ lib/service_actor/base.rb
@@ -6,18 +6,18 @@ module ServiceActor
   module Base
     def self.included(base)
       # Essential mechanics
-      base.include(ServiceActor::Core)
-      base.include(ServiceActor::Raisable)
-      base.include(ServiceActor::Attributable)
-      base.include(ServiceActor::Playable)
+      # base.include(ServiceActor::Core)
+      # base.include(ServiceActor::Raisable)
+      # base.include(ServiceActor::Attributable)
+      # base.include(ServiceActor::Playable)
 
-      # Extra concerns
-      base.include(ServiceActor::TypeCheckable)
-      base.include(ServiceActor::NilCheckable)
-      base.include(ServiceActor::Conditionable)
-      base.include(ServiceActor::Collectionable)
-      base.include(ServiceActor::Defaultable)
-      base.include(ServiceActor::Failable)
+      # # Extra concerns
+      # base.include(ServiceActor::TypeCheckable)
+      # base.include(ServiceActor::NilCheckable)
+      # base.include(ServiceActor::Conditionable)
+      # base.include(ServiceActor::Collectionable)
+      # base.include(ServiceActor::Defaultable)
+      # base.include(ServiceActor::Failable)
     end
   end
 end

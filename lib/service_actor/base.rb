a4b4acbe3340dca80b46e5f0af7a5b582f304d7c
Anton <profox.rus@gmail.com>
2022-09-22 16:03:18 +0300

    Attempt

--- lib/service_actor/base.rb
+++ lib/service_actor/base.rb
@@ -2,20 +2,22 @@
 
 require "service_actor/support/loader"
 
-module ServiceActor::Base
-  def self.included(base)
-    # Essential mechanics
-    base.include(ServiceActor::Core)
-    base.include(ServiceActor::Raisable)
-    base.include(ServiceActor::Attributable)
-    base.include(ServiceActor::Playable)
+module ServiceActor # rubocop:disable Style/ClassAndModuleChildren
+  module Base
+    def self.included(base)
+      # Essential mechanics
+      base.include(ServiceActor::Core)
+      base.include(ServiceActor::Raisable)
+      base.include(ServiceActor::Attributable)
+      base.include(ServiceActor::Playable)
 
-    # Extra concerns
-    base.include(ServiceActor::TypeCheckable)
-    base.include(ServiceActor::NilCheckable)
-    base.include(ServiceActor::Conditionable)
-    base.include(ServiceActor::Collectionable)
-    base.include(ServiceActor::Defaultable)
-    base.include(ServiceActor::Failable)
+      # Extra concerns
+      base.include(ServiceActor::TypeCheckable)
+      base.include(ServiceActor::NilCheckable)
+      base.include(ServiceActor::Conditionable)
+      base.include(ServiceActor::Collectionable)
+      base.include(ServiceActor::Defaultable)
+      base.include(ServiceActor::Failable)
+    end
   end
 end

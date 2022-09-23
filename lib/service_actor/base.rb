5298d4f9012e9ef340348dec8bcc95f87c7b627f
Anton <profox.rus@gmail.com>
2022-09-23 13:25:23 +0300

    Attempt

--- lib/service_actor/base.rb
+++ lib/service_actor/base.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-
-
 # Core
 require "service_actor/core"
 require "service_actor/raisable"
@@ -22,20 +20,22 @@ require "service_actor/collectionable"
 require "service_actor/defaultable"
 require "service_actor/failable"
 
-module ServiceActor::Base
-  def self.included(base)
-    # Essential mechanics
-    base.include(ServiceActor::Core)
-    base.include(ServiceActor::Raisable)
-    base.include(ServiceActor::Attributable)
-    base.include(ServiceActor::Playable)
+module ServiceActor
+  module Base
+    def self.included(base)
+      # Essential mechanics
+      base.include(ServiceActor::Core)
+      base.include(ServiceActor::Raisable)
+      base.include(ServiceActor::Attributable)
+      base.include(ServiceActor::Playable)
 
-    # Extra concerns
-    base.include(ServiceActor::TypeCheckable)
-    base.include(ServiceActor::NilCheckable)
-    base.include(ServiceActor::Conditionable)
-    base.include(ServiceActor::Collectionable)
-    base.include(ServiceActor::Defaultable)
-    base.include(ServiceActor::Failable)
+      # Extra concerns
+      base.include(ServiceActor::TypeCheckable)
+      base.include(ServiceActor::NilCheckable)
+      base.include(ServiceActor::Conditionable)
+      base.include(ServiceActor::Collectionable)
+      base.include(ServiceActor::Defaultable)
+      base.include(ServiceActor::Failable)
+    end
   end
 end

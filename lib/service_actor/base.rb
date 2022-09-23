74e2f10164de0b809b0d638c1713e21b9b1d110a
Anton <profox.rus@gmail.com>
2022-09-23 13:21:50 +0300

    Attempt

--- lib/service_actor/base.rb
+++ lib/service_actor/base.rb
@@ -1,6 +1,26 @@
 # frozen_string_literal: true
 
-require "service_actor/support/loader"
+
+
+# Core
+require "service_actor/core"
+require "service_actor/raisable"
+require "service_actor/attributable"
+require "service_actor/playable"
+require "service_actor/result"
+
+# Exceptions
+require "service_actor/error"
+require "service_actor/failure"
+require "service_actor/argument_error"
+
+# Concerns
+require "service_actor/type_checkable"
+require "service_actor/nil_checkable"
+require "service_actor/conditionable"
+require "service_actor/collectionable"
+require "service_actor/defaultable"
+require "service_actor/failable"
 
 module ServiceActor::Base
   def self.included(base)
